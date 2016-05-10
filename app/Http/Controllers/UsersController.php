5542bcdf65d540dd943bae982dcc0b73ac1f369d
Ricardo Mendes <ricardo.s.c.mendes@gmail.com>
2016-05-10 16:49:34 +0100

    Finishes user validation

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -37,7 +37,7 @@ class UsersController extends Controller
     /*
      * Atualiza utilizador
      */
-    public function postUser(Request $request, $identification)
+    public function putUser(Request $request, $identification)
     {
         $input = $request->except('_token');
         // TODO: Falta verificar as permissoes do user para editar este objeto (se admin ou utilizador)
@@ -46,28 +46,33 @@ class UsersController extends Controller
             \App::abort(404);
             return NULL;
         }
-        $user->update($input);
+
+        if($this->runValidation($input, $identification)) {
+            $user->update($input);
+            return $user;
+        }
+
     }
 
     /*
      * Regista utilizador na base de dados
      */
-    public function putUser(Request $request)
+    public function postUser(Request $request)
     {
         $input = $request->except('_token');
-        if($this->putValidation($input)) {
+        if($this->runValidation($input, NULL)) {
             $user = User::create([
                 "username" => $input['username'],
                 "password" => Hash::make($input['password']),
                 "student_id" => $input['student_id'],
                 "email" => $input['email']
             ]);
-            \Auth::login($user); // Login the new user.
-            return redirect('home')->with('message', 'Bem vindo ao Clube de Programação!'); // return to web.app/home with success message
+            return $user;
         }
-        // user didn't validate, return to old page.
 
-        return \Redirect::back()->with('error', 'Algo de errado aconteceu.')->withErrors(); // error message bag.
+        //Validation failed
+        \App::abort(404);
+        return NULL;
     }
 
 
@@ -86,14 +91,29 @@ class UsersController extends Controller
      * Valida as informações vindas do utilizador
      * DOCS: https://laravel.com/docs/5.2/validation
      */
-    private function putValidation($input)
+    private function runValidation($input, $id)
     {
-        $validationarray = [
-            'username' => 'required|unique:users',
-            'email' => 'required|email|unique:users',
-            'student_id' => ["required|unique:users", "Regex:/^([0-9]{10})$/"], // obriga a que todos os números sejam uc201xxxxxxx ou a201xxxxxxx
-            'password' => 'required|confirmed', // o "confirmed" necessita de um text input com o nome de password_confirmation, para verificar se a password é igual nos dois sítios.
-        ];
+        if($id == NULL) { //post (creation)
+            $validationarray = [
+                'username' => 'required|unique:users',
+                'email' => 'required|email|unique:users',
+                'student_id' => ["required|unique:users", "Regex:/^([0-9]{10})$/"], // obriga a que todos os números sejam uc201xxxxxxx ou a201xxxxxxx
+                'password' => 'required|confirmed', // o "confirmed" necessita de um text input com o nome de password_confirmation, para verificar se a password é igual nos dois sítios.
+                'name' => 'required',
+                'version_control' => 'url',
+                'avatar' => 'url'
+            ];
+        } else { //put (update)
+            $validationarray = [
+                'username' => 'unique:users',
+                'email' => 'email|unique:users',
+                'student_id' => ["unique:users", "Regex:/^([0-9]{10})$/"], // obriga a que todos os números sejam uc201xxxxxxx ou a201xxxxxxx
+                'password' => 'confirmed', // o "confirmed" necessita de um text input com o nome de password_confirmation, para verificar se a password é igual nos dois sítios.
+                'version_control' => 'url',
+                'avatar' => 'url'
+            ];
+        }
+
         $validator = \Validator::make($input, $validationarray);
         return $validator->passes();
     }
