622e96b26c4f7aca0110e64d5fe5d4d54c27ce02
Francisco Couceiro <couceiro.f@gmail.com>
2016-08-01 10:54:07 +0200

    Downloads all assets and libs (are now served from the server's file system)

--- public/app/components/events/eventsController.js
+++ public/app/components/events/eventsController.js
@@ -5,12 +5,12 @@
         // Should retrieve this form the API
         $scope.events = [
             {
-                thumb : "http://i.istockimg.com/file_thumbview_approve/97860153/3/stock-photo-97860153-business-man-hand-working-on-laptop-computer-with-digital-layer.jpg",
+                thumb : "assets/img/event1.jpg",
                 title : "Event #1",
                 subtitle : "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit"
             },
             {
-                thumb : "http://i.istockimg.com/file_thumbview_approve/97889747/3/stock-photo-97889747-let-s-get-started-word-on-white-ring-binder-notebook.jpg",
+                thumb : "assets/img/event2.jpg",
                 title : "Event #2",
                 subtitle : "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit"
             }
--- public/app/components/projects/projectsController.js
+++ public/app/components/projects/projectsController.js
@@ -5,17 +5,17 @@
         // Should retrieve this form the API
         $scope.projects = [
             {
-                thumb : "http://i4.istockimg.com/file_thumbview_approve/83967207/5/stock-photo-83967207-hud-hologram-futuristic-colorful-abstract-background.jpg",
+                thumb : "assets/img/project1.jpg",
                 title : "Project #1",
                 subtitle : "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit"
             },
             {
-                thumb : "http://i3.istockimg.com/file_thumbview_approve/76611677/5/stock-photo-76611677-abstract-background-science-technology.jpg",
+                thumb : "assets/img/project2.jpg",
                 title : "Project #2",
                 subtitle : "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit"
             },
             {
-                thumb : "http://i2.istockimg.com/file_thumbview_approve/75447797/5/stock-photo-75447797-optical-fiber-abstract.jpg",
+                thumb : "assets/img/project3.jpg",
                 title : "Project #3",
                 subtitle : "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit"
             }
